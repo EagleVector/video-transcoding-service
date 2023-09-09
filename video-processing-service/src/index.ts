242b19c10a47b15ec05c8ce74d95d7ac40553102
EagleVector <kumarsubhambili3@gmail.com>
2023-09-09 22:56:19 +0530

    processing videos locally to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const port = process.env.PORT || 3000;
+
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file Path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Processing finished successfully.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occured: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
 app.listen(port, () => {
