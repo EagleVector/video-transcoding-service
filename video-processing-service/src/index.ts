16ded396b978bd578ffc75e2652c02fa36d3caa3
EagleVector <kumarsubhambili3@gmail.com>
2023-09-15 23:46:46 +0530

    Checking the status of the uploaded video and assigning it to the queue

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { isVideoNew, setVideo } from "./firestore";
 import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
 
 setupDirectories();
@@ -22,8 +23,19 @@ app.post("/process-video", async (req, res) => {
     return res.status(400).send('Bad Request: Missing Filename')
   }
 
-  const inputFileName = data.name;
+  const inputFileName = data.name; // Format of <UID>-<DATE>.<EXTENSION>
   const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split('.')[0];
+
+  if (!isVideoNew(videoId)) {
+    return res.status(400).send('Bad Request: Video already processing or processed')
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split('-')[0],
+      status: "processing"
+    })
+  }
 
   // Download the raw video from Cloud Storage
   await downloadRawVideo(inputFileName);
@@ -43,6 +55,11 @@ app.post("/process-video", async (req, res) => {
 
   // Upload the processed video to Cloud Storage
   await uploadProcessedVideo(outputFileName);
+
+  await setVideo(videoId, {
+    status: 'processed',
+    filename: outputFileName
+  })
 
   await Promise.all([
       deleteRawVideo(inputFileName),
