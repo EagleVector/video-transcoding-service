e9dc99f2763ae49f33b3ababebc8f4ac4098ef7c
EagleVector <kumarsubhambili3@gmail.com>
2023-09-16 00:21:03 +0530

    Fetching all the video info from firestore database backend

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -12,6 +12,18 @@ const storage = new Storage();
 
 const rawVideoBucketName = "cherry-raw-videos";
 
+const videoCollectionId = 'videos';
+
+export interface Video {
+  id? : string;
+  uid? : string;
+  filename?: string;
+  status?: string;
+  title?: string;
+  description?: string
+}
+
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -48,4 +60,9 @@ export const generateUploadURL = onCall({maxInstances: 1}, async (request) => {
   });
 
   return { url, fileName };
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot = await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
